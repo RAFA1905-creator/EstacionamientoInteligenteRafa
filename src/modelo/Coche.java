f9249c7c77722bcea07845815a575ffa2fcd44c1
rafaelgaln <rafael.galn@educa.madrid.org>
2025-10-21 11:17:17 +0200

    Atributos de las clases de Modelo. Metodos desalojarCocheNormal y esVip para Estacionamiento y Coche respectivamente.

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,4 +1,12 @@
 package modelo;
 
 public class Coche {
+
+    private String nombre;
+    private boolean vip;
+    private Estacionamiento estacionamiento;
+
+    public boolean esVip() {
+        return vip;
+    }
 }
--- src/modelo/Estacionamiento.java
+++ src/modelo/Estacionamiento.java
@@ -1,4 +1,21 @@
 package modelo;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Estacionamiento {
+
+    private final int capacidadMaxima = 5;
+    private final Semaphore semaforo = new Semaphore(capacidadMaxima);
+    private final ArrayList<Coche> cochesAparcados = new ArrayList<>();
+
+    private void desalojarCocheNormal(Coche cocheVip) {
+        for (Coche coche : cochesAparcados) {
+            if (!coche.esVip()) {
+                cochesAparcados.remove(coche);
+                semaforo.release();
+                System.out.println(coche + " fue desalojado por " + cocheVip);
+                break;
+            }
+        }
+    }
 }
