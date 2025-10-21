9e3772e1b6fd5e3782eb795cd0d651c8dc0a1ae1
rafaelgaln <rafael.galn@educa.madrid.org>
2025-10-21 13:32:32 +0200

    Metodo "run" de Coche.

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -9,4 +9,18 @@ public class Coche {
     public boolean esVip() {
         return vip;
     }
+
+    public void run() {
+        boolean aparcado = estacionamiento.entrar(this);
+        if (aparcado == true) {
+            try {
+                int tiempo = 2000 + ((int) (Math.random() * 4000));
+                Thread.sleep(tiempo);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                //TODO: SALIR DEL ESTACIONAMIENTO
+            }
+        }
+    }
 }
--- src/modelo/Estacionamiento.java
+++ src/modelo/Estacionamiento.java
@@ -1,4 +1,5 @@
 package modelo;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
@@ -8,13 +9,43 @@ public class Estacionamiento {
     private final Semaphore semaforo = new Semaphore(capacidadMaxima);
     private final ArrayList<Coche> cochesAparcados = new ArrayList<>();
 
-    private void desalojarCocheNormal(Coche cocheVip) {
-        for (Coche coche : cochesAparcados) {
-            if (!coche.esVip()) {
-                cochesAparcados.remove(coche);
-                semaforo.release();
-                System.out.println(coche + " fue desalojado por " + cocheVip);
-                break;
+    public boolean entrar(Coche coche) {
+        boolean aparcado = false;
+
+        if (coche.esVip() == true) {
+            try {
+                semaforo.acquire();
+                synchronized (cochesAparcados) {
+                    if (cochesAparcados.size() >= capacidadMaxima) {
+                        desalojarCocheNormal(coche);
+                        cochesAparcados.add(coche);
+
+                        aparcado = true;
+                    }
+                }
+            } catch(Exception e) {}
+        } else if (coche.esVip() == false) {
+            try {
+                if (semaforo.tryAcquire(5)) {
+                    cochesAparcados.add(coche);
+
+                    aparcado = true;
+                } else {
+                    System.out.println(coche + " ha abandonado la cola del estacionamiento.");
+                }
+            } catch(Exception e) {}
+        }
+        return aparcado;
+    }
+    public void desalojarCocheNormal(Coche cocheVip) {
+        synchronized (cochesAparcados) {
+            for (Coche coche : cochesAparcados) {
+                if (!coche.esVip()) {
+                    cochesAparcados.remove(coche);
+                    semaforo.release();
+                    System.out.println(coche + " fue desalojado por " + cocheVip);
+                    break;
+                }
             }
         }
     }
